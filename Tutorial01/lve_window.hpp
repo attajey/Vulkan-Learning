5698d763f2a4f40349c246ed9c2cf9c40a4924c1
Atta Jirofty <jirofty76@gmail.com>
2023-02-05 22:04:44 -0500

    Update lve_window.hpp

--- Tutorial01/lve_window.hpp
+++ Tutorial01/lve_window.hpp
@@ -4,13 +4,16 @@
 #include <GLFW/glfw3.h>
 #include <string>
 
-namespace lve 
+namespace lve
 {
 	class LveWindow
 	{
 	public:
 		LveWindow(int w, int h, std::string name);
 		~LveWindow();
+
+		LveWindow(const LveWindow&) = delete;
+		LveWindow& operator=(const LveWindow&) = delete; //RAII Resource Acquisition Is Initialization
 
 		bool shouldClose() { return glfwWindowShouldClose(window); }
 

d97d6a29f032a2b96f69f78c724f10475f37e3df
Atta Jirofty <jirofty76@gmail.com>
2023-02-14 20:34:34 -0500

    Tutorial04 Init

---
+++ Tutorial04/first_app.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "lve_window.hpp"
+#include "lve_pipeline.hpp"
+#include "lve_device.hpp"
+
+namespace lve 
+{
+	class FirstApp
+	{
+	public:
+		static constexpr int WIDTH = 800;
+		static constexpr int HEIGHT = 600;
+
+		//FirstApp();
+		//~FirstApp();
+		void run();
+
+	private:
+		LveWindow lveWindow{ WIDTH, HEIGHT, "Hail Vulkan!" };
+		LveDevice lveDevice{ lveWindow };
+		LvePipeline lvePipeline{lveDevice, "simple_shader.vert.spv", "simple_shader.frag.spv", LvePipeline::defaultPipelineConfigInfo(WIDTH, HEIGHT)};
+
+	};
+
+}

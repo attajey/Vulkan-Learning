8cfdf6366e92e175f872b5da9be2688fef684be6
Atta Jirofty <jirofty76@gmail.com>
2023-02-24 12:59:27 -0500

    Added Tutorial06

--- Tutorial06/first_app.cpp
+++ Tutorial06/first_app.cpp
@@ -5,6 +5,7 @@ namespace lve
 {
 	FirstApp::FirstApp()
 	{
+		loadModels();
 		createPipelineLayout();
 		createPipeline();
 		createCommandBuffers();
@@ -25,6 +26,19 @@ namespace lve
 		vkDeviceWaitIdle(lveDevice.device());
 
 	}
+
+	void FirstApp::loadModels()
+	{
+		std::vector<LveModel::Vertex> vertices
+		{
+			{{0.0f, -0.5f}},
+			{{0.5f, 0.5f}},
+			{{-0.5f,0.5f}}
+		};
+
+		lveModel = std::make_unique<LveModel>(lveDevice, vertices);
+	}
+
 	void FirstApp::createPipelineLayout()
 	{
 		VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
@@ -86,7 +100,8 @@ namespace lve
 			vkCmdBeginRenderPass(commandBuffers[i], &renderpassInfo, VK_SUBPASS_CONTENTS_INLINE);
 
 			lvePipeline->bind(commandBuffers[i]);
-			vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
+			lveModel->bind(commandBuffers[i]);
+			lveModel->draw(commandBuffers[i]);
 
 			vkCmdEndRenderPass(commandBuffers[i]);
 			if (vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS)

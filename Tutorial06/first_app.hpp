c6f3f09bac6de20acf0648ed638d4bdc66727963
Atta Jirofty <jirofty76@gmail.com>
2023-02-24 11:40:13 -0500

    Init Tutorial06

---
+++ Tutorial06/first_app.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "lve_device.hpp"
+#include "lve_pipeline.hpp"
+#include "lve_swap_chain.hpp"
+#include "lve_window.hpp"
+
+#include <memory>
+#include <vector>
+
+namespace lve
+{
+	class FirstApp
+	{
+	public:
+		static constexpr int WIDTH = 800;
+		static constexpr int HEIGHT = 600;
+
+		FirstApp();
+		~FirstApp();
+
+		FirstApp(const FirstApp&) = delete;
+		FirstApp& operator=(const FirstApp&) = delete; //RAII Resource Acquisition Is Initialization
+
+
+		void run();
+
+	private:
+		void createPipelineLayout();
+		void createPipeline();
+		void createCommandBuffers();
+		void drawFrame();
+
+		LveWindow lveWindow{ WIDTH, HEIGHT, "Hail Vulkan!" };
+		LveDevice lveDevice{ lveWindow };
+		LveSwapChain lveSwapChain{ lveDevice, lveWindow.getExtent() };
+		std::unique_ptr<LvePipeline> lvePipeline;
+		VkPipelineLayout pipelineLayout;
+		std::vector<VkCommandBuffer> commandBuffers;
+
+	};
+
+}

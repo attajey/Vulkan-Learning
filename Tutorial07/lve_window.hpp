8c26f998fec2721057439921d542758fcca9a4ca
Atta Jirofty <jirofty76@gmail.com>
2023-03-07 08:42:45 -0500

    Added Tutorial 07 08
    Up until half of tutorial 08. You can resize

---
+++ Tutorial07/lve_window.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN // Signal that we want glfw to also include the Vulkan headers with it
+#include <GLFW/glfw3.h>
+#include <string>
+
+namespace lve
+{
+	class LveWindow
+	{
+	public:
+		LveWindow(int w, int h, std::string name);
+		~LveWindow();
+
+		LveWindow(const LveWindow&) = delete;
+		LveWindow& operator=(const LveWindow&) = delete; //RAII Resource Acquisition Is Initialization
+
+		bool shouldClose() { return glfwWindowShouldClose(window); }
+		VkExtent2D getExtent() { return { static_cast<uint32_t>(width), static_cast<uint32_t>(height) }; }
+		bool wasWindowResized() { return framebufferResized; }
+		void resetWindowResizedFlag() { framebufferResized = false; }
+
+		void createWindowSurface(VkInstance instance, VkSurfaceKHR* surface);
+
+	private:
+		static void framebufferResizeCallback(GLFWwindow* window, int width, int height);
+		void initWindow();
+
+		int width;
+		int height;
+		bool framebufferResized = false;
+
+		std::string windowName;
+		GLFWwindow* window;
+	};
+}
